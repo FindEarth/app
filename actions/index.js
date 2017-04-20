2fa27f01778920e0f298160eb2dd196d010e6639
Matias Tucci <matiastucci@gmail.com>
2017-04-20 04:44:46 -0400

    use fetch

--- actions/index.js
+++ actions/index.js
@@ -5,7 +5,6 @@ import {
 } from '../constants/ActionTypes'
 import { createAction } from 'redux-actions'
 import Api from '../constants/Api'
-import trae from 'trae'
 
 export const fetchingPersonList = createAction(FETCHING_PERSON_LIST)
 export const errorFetching = createAction(ERROR_FETCHING)
@@ -13,13 +12,13 @@ export const successFetching = createAction(SUCCESS_FETCHING)
 
 export function fetchPersonList() {
   return function(dispatch) {
-    dispatch(fetchingPersonList())
     const url = `${Api.baseUrl}${Api.personEndpoint}`
-    trae.get(url)
-      .then((response) => {
-        dispatch(successFetching(response))
-      })
-      .catch((err) => {
+    dispatch(fetchingPersonList())
+    fetch(url)
+      .then(res => res.json().then(json => {
+        dispatch(successFetching(json))
+      }))
+      .catch(err => {
         dispatch(errorFetching(err.message))
       })
   }
