309a4e99ff75132aac024d0989573c205381a1c4
Martin Carrera <mcarrera.92@gmail.com>
2017-04-23 13:53:21 -0300

    Add form inputs

--- components/PersonCreateView.js
+++ components/PersonCreateView.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import { ScrollView } from 'react-native'
 import PropTypes from 'prop-types'
+import { FormLabel, FormInput, Button } from 'react-native-elements'
+import Colors from './../constants/Colors'
 
 function PersonCreateView({styles}) {
   return (
     <ScrollView
       style={styles.container}
+    >
+    <FormLabel>{'Tus Datos'}</FormLabel>
+    <FormInput placeholder='Nombre' />
+    <FormInput placeholder='Email' />
+    <FormInput placeholder='Telefono' />
+    <FormLabel>{'Datos de la persona perdida'}</FormLabel>
+    <FormInput placeholder='Nombre' />
+    <FormInput placeholder='Email' />
+    <FormInput placeholder='Sexo' />
+    <FormLabel>{'Ultima vez visto'}</FormLabel>
+    <FormInput placeholder='MM/DD/YYYY' />
+    <Button
+      raised
+      title='Enviar'
+      backgroundColor={Colors.tintColor}
+      color={Colors.white}
     />
+    </ScrollView>
   )
 }
 
