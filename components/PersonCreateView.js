265b6e09672e88115b3390e50b85398f00a3e961
Matias Tucci <matiastucci@gmail.com>
2017-04-26 11:40:12 -0400

    improve form

--- components/PersonCreateView.js
+++ components/PersonCreateView.js
@@ -1,30 +1,72 @@
 import React from 'react'
 import { ScrollView } from 'react-native'
 import PropTypes from 'prop-types'
-import { FormLabel, FormInput, Button } from 'react-native-elements'
+import { FormLabel, FormInput, Button, CheckBox } from 'react-native-elements'
 import Colors from './../constants/Colors'
 
 function PersonCreateView({styles}) {
   return (
-    <ScrollView
-      style={styles.container}
-    >
-    <FormLabel>{'Tus Datos'}</FormLabel>
-    <FormInput placeholder='Nombre' />
-    <FormInput placeholder='Email' />
-    <FormInput placeholder='Telefono' />
-    <FormLabel>{'Datos de la persona perdida'}</FormLabel>
-    <FormInput placeholder='Nombre' />
-    <FormInput placeholder='Email' />
-    <FormInput placeholder='Sexo' />
-    <FormLabel>{'Ultima vez visto'}</FormLabel>
-    <FormInput placeholder='MM/DD/YYYY' />
-    <Button
-      raised
-      title='Enviar'
-      backgroundColor={Colors.tintColor}
-      color={Colors.white}
-    />
+    <ScrollView style={styles.container}>
+
+      <FormLabel>{'Tus Datos'}</FormLabel>
+
+      <FormInput
+        placeholder={'Nombre'}
+        autoCapitalize={'words'}
+        autoFocus={true}
+        autoCorrect={false}
+      />
+
+      <FormInput
+        placeholder={'Email'}
+        autoCapitalize={'none'}
+        autoCorrect={false}
+        keyboardType={'email-address'}
+      />
+
+      <FormInput
+        placeholder={'Telefono'}
+        autoCorrect={false}
+        keyboardType={'phone-pad'}
+      />
+
+      <FormLabel>{'Datos de la persona perdida'}</FormLabel>
+
+      <FormInput
+        placeholder={'Nombre'}
+        autoCapitalize={'words'}
+        autoFocus={true}
+        autoCorrect={false}
+      />
+
+      <FormInput
+        placeholder={'Edad'}
+        keyboardType={'numeric'}
+      />
+
+      <FormLabel>{'Sexo'}</FormLabel>
+      <CheckBox
+        center
+        title='F'
+        checkedIcon='dot-circle-o'
+        uncheckedIcon='circle-o'
+      />
+      <CheckBox
+        center
+        title='M'
+        checkedIcon='dot-circle-o'
+        uncheckedIcon='circle-o'
+      />
+
+      <FormLabel>{'Ultima vez visto'}</FormLabel>
+      <FormInput placeholder='MM/DD/YYYY' />
+
+      <Button
+        raised
+        title='Enviar'
+        backgroundColor={Colors.tintColor}
+        color={Colors.white}
+      />
     </ScrollView>
   )
 }
